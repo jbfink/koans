fcd9b0da2d90ba9331fb036f88aec1f5d952e012
John Fink <john.fink@gmail.com>
2010-08-27 12:43:33 -0400

    finished test_use_double_quotes_to_create_strings_with_single_quotes.

--- about_strings.rb
+++ about_strings.rb
@@ -18,7 +18,7 @@ class AboutStrings < EdgeCase::Koan
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
-    assert_equal __, string
+    assert_equal "Don't", string
   end
 
   def test_use_backslash_for_those_hard_cases
