c733686792726606719be98b508174e677ed5cbc
John Fink <jbfink@zamenhof>
2010-08-28 09:39:50 -0400

    finished test_strings_can_be_split.

--- about_strings.rb
+++ about_strings.rb
@@ -149,24 +149,24 @@ EOS
 
   in_ruby_version("1.8") do
     def test_in_ruby_1_8_single_characters_are_represented_by_integers
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal 97, ?a
+      assert_equal true, ?a == 97
 
-      assert_equal __, ?b == (?a + 1)
+      assert_equal true, ?b == (?a + 1)
     end
   end
 
   in_ruby_version("1.9") do
     def test_in_ruby_1_8_single_characters_are_represented_by_strings
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal "a", ?a
+      assert_equal false, ?a == 97
     end
   end
 
   def test_strings_can_be_split
     string = "Sausage Egg Cheese"
     words = string.split
-    assert_equal [__, __, __], words
+    assert_equal ["Sausage","Egg","Cheese"], words
   end
 
   def test_strings_can_be_split_with_different_patterns
