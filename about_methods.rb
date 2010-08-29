92740146ebe1233f6c6b2c5813041cb0a30b6086
John Fink <jbfink@zamenhof>
2010-08-28 22:09:32 -0400

    finished test_sometimes_missing_parenthesis_are_ambiguous has expanded your awareness.

--- about_methods.rb
+++ about_methods.rb
@@ -12,13 +12,13 @@ class AboutMethods < EdgeCase::Koan
 
   def test_calling_global_methods_without_parenthesis
     result = my_global_method 2, 3
-    assert_equal __, result
+    assert_equal 5, result
   end
 
   # (NOTE: We are Using eval below because the example code is
   # considered to be syntactically invalid).
   def test_sometimes_missing_parenthesis_are_ambiguous
-    eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK
+    eval "assert_equal 5, my_global_method(2,3)" # ENABLE CHECK
     #
     # Ruby doesn't know if you mean:
     #
