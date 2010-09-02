cd10c96c82fc7a28cf44f9dd56909933e37a9c2b
John Fink <jbfink@zamenhof>
2010-09-01 22:06:01 -0400

    finished triangle.rb, Thinking AboutTriangleAssignment.

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+if a == b and b == c
+then return :equilateral
+elsif a == b or b == c or c == a
+then return :isosceles
+else return :scalene
   # WRITE THIS CODE
 end
-
+end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
