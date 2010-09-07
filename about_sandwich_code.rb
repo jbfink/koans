31390c7c3ce5f9e31cda5cc6ca8cf517db0b0f35
John Fink <jbfink@zamenhof>
2010-09-06 23:21:23 -0400

    finished test_open_handles_the_file_sandwich_when_given_a_block.

--- about_sandwich_code.rb
+++ about_sandwich_code.rb
@@ -108,7 +108,7 @@ class AboutUsingBlocks < EdgeCase::Koan
   end
 
   def test_open_handles_the_file_sandwich_when_given_a_block
-    assert_equal __, count_lines3("example_file.txt")
+    assert_equal 4, count_lines3("example_file.txt")
   end
 
 end
