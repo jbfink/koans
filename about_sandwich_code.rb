563156be520d98bb76fa41d7fdabab0d0c8e1bae
John Fink <jbfink@zamenhof>
2010-09-06 21:08:06 -0400

    finished test_counting_lines2.

--- about_sandwich_code.rb
+++ about_sandwich_code.rb
@@ -74,7 +74,7 @@ class AboutUsingBlocks < EdgeCase::Koan
   end
 
   def test_counting_lines2
-    assert_equal __, count_lines2("example_file.txt")
+    assert_equal 4, count_lines2("example_file.txt")
   end
 
   # ------------------------------------------------------------------
