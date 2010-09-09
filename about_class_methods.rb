d1af7b99baafe3a6f9742eafc80003af4e3f8a27
John Fink <john.fink@gmail.com>
2010-09-09 15:39:35 -0400

    Finished test_heres_an_easy_way_to_call_class_methods_from_instance_methods.

--- about_class_methods.rb
+++ about_class_methods.rb
@@ -141,7 +141,7 @@ class AboutClassMethods < EdgeCase::Koan
   end
 
   def test_heres_still_another_way_to_write_class_methods
-    assert_equal __, Dog.another_class_method
+    assert_equal :still_another_way, Dog.another_class_method
   end
 
   # THINK ABOUT IT:
@@ -164,7 +164,7 @@ class AboutClassMethods < EdgeCase::Koan
 
   def test_heres_an_easy_way_to_call_class_methods_from_instance_methods
     fido = Dog.new
-    assert_equal __, fido.class.another_class_method
+    assert_equal :still_another_way, fido.class.another_class_method
   end
 
 end
